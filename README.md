2ccf2938eddb094ec1427137255fedda94a2f2e8
KiwiCandy-ai <206121829+KiwiCandy-ai@users.noreply.github.com>
2025-07-13 14:17:46 +0100

    Add thankyou card

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Frontend Mentor - Interactive rating component
 
 ![Design preview for the Interactive rating component coding challenge](./preview.jpg)
@@ -93,6 +92,3 @@ We love receiving feedback! We're always looking to improve our challenges and o
 This challenge is completely free. Please share it with anyone who will find it useful for practice.
 
 **Have fun building!** 🚀
-=======
-# fem-rating-component
->>>>>>> remotes/origin/main
--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ container.appendChild(heading)
 
 const text = document.createElement('div')
 text.classList.add('text')
-text.textContent = 'Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering.'
+text.textContent = 'Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!'
 container.appendChild(text)
 
 const numbers = document.createElement('div')
@@ -33,16 +33,57 @@ for (let i = 1; i < 6; i++) {
     numIcon.textContent = `${i}`
     numIcon.classList.add('num')
     numbers.appendChild(numIcon)
-    numIcon.addEventListener('click', function(e) {
-        const targetElem = e.target
-        targetElem.classList.add('selected')
-        
-    })
+    numIcon.addEventListener('click', selectNum) 
+    
 }
 
 const submit = document.createElement('button')
 submit.textContent = 'SUBMIT'
 submit.classList.add('submit')
+submit.addEventListener('click', submitResult)
 container.appendChild(submit)
 
+let result = 0
 
+function selectNum(e) {
+    const numIcon = document.querySelectorAll('.selected')
+numIcon.forEach(function (elem) {
+    elem.classList.remove('selected')
+});
+const targetElem = e.target
+        targetElem.classList.add('selected')
+       result = targetElem.textContent
+        return result
+    }
+
+function submitResult() {
+    const numIcon = document.querySelector('.selected')
+    console.log(numIcon)
+    if (numIcon.classList.contains('selected')) {
+        let child = container.lastElementChild 
+            while (child) {
+                container.removeChild(child)
+                child = container.lastElementChild
+            } 
+            const thanks = document.createElement('img')
+            thanks.classList.add('thanks')
+            thanks.src = 'https://raw.githubusercontent.com/KiwiCandy-ai/fem-rating-component/36b9fbd8dff5611971fc385b19812a2d61508ac8/images/illustration-thank-you.svg'
+            container.appendChild(thanks)
+            const displayResult = document.createElement('div')
+            displayResult.classList.add('result')
+            displayResult.textContent = `You selected ${result} out of 5`
+            container.appendChild(displayResult)
+            const heading = document.createElement('div')
+            heading.textContent = 'Thank You!'
+            heading.classList.add('heading')
+            container.appendChild(heading)
+            const para = document.createElement('div')
+            para.textContent = 'We appreciate you taking the time to give us a rating. If you ever need more support, dont hesitate to get in touch!'
+            para.classList.add('text')
+            container.appendChild(para)
+            container.classList.add('second')
+        } else {
+                
+            } 
+        }
+    
